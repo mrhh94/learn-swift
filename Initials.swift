b778eb7c7068fd7c1ad37c2d7fe234df26673f5f
mrhh94 <34717736+mrhh94@users.noreply.github.com>
2020-04-11 11:23:47 -0500

    Create Initials.swift

---
+++ Initials.swift
@@ -0,0 +1,8 @@
+// Fun Fact: I am lazy
+print("M       M  H       H")
+print("M M   M M  H       H")
+print("M M   M M  H       H")
+print("M   M   M  H H H H H")
+print("M       M  H       H")
+print("M       M  H       H")
+print("M       M  H       H")
